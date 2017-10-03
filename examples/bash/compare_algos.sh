95c38ec848c32bd992e5c78a04c0147fc765c747
Talha Imran <talha_imran@mentor.com>
2017-10-03 19:33:11 +0500

    Add support for testing multiple algos against each other.
    	* examples/bash/compare_algos.sh: New.
    	* examples/make-graph.py: Update to handle multiple algo tuples
    	* examples/bash/compare_slic_customslic.sh: Remove.
    	* examples/bash/update_customslic.shL: Likewise.

---
+++ examples/bash/compare_algos.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+# Copyright (c) 2016, David Stutz
+# Contact: david.stutz@rwth-aachen.de, davidstutz.de
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Helper functions...
+
+# list_include_item "10 11 12" "2"
+function list_include_item {
+  declare -a list=("${!1}")
+  local item="$2"
+  if [[ ${list[@]} =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    # yes, list include item
+    result=0
+  else
+    result=1
+  fi
+  return $result
+}
+
+# Main program...
+# Supposed to be run from within examples/.
+
+#TUPLES_TO_UPDATE_LIST=("customslic-1-tuple" "customslic-10-tuple")
+
+SUPERPIXELS=("200" "300" "400" "600" "800" "1000" "1200" "1400" "1600" "1800" "2000" "2400" "2800" "3200" "3600" "4000" "4600" "5200")
+NUM_ITERATIONS_LIST=("1")
+
+# Create algo_tuples list and print it.
+algo_tuples_list="slic-10"
+for NUM_ITERATIONS in "${NUM_ITERATIONS_LIST[@]}"
+do
+	algo_tuple="customslic-$NUM_ITERATIONS-tuple"
+	algo_tuples_list+=(${algo_tuple})
+done
+echo "***Algo tuples:***"
+printf '%s\n' "${algo_tuples_list[@]}"
+echo 
+
+# Make a string of tuples as a Comma separated list
+algo_tuples_string=""
+for algo_tuple in "${algo_tuples_list[@]}"
+do
+	if [ -z "$algo_tuples_string" ]; then
+    	algo_tuples_string="${algo_tuple}"
+    else
+    	algo_tuples_string="${algo_tuples_string},${algo_tuple}"
+	fi
+done
+
+# Update all tuples if TUPLES_TO_UPDATE_LIST is not set.
+if [ -z ${TUPLES_TO_UPDATE_LIST} ]; then 
+	TUPLES_TO_UPDATE_LIST="${algo_tuples_list[@]}"
+fi
+
+
+# Main loop
+for algo_tuple in "${algo_tuples_list[@]}"
+do
+	# Check if we want to run this tuple again
+	if list_include_item TUPLES_TO_UPDATE_LIST[@] "${algo_tuple}" ; then
+		echo
+		echo
+		echo "**Working on ${algo_tuple}"
+	else 
+	  	continue
+	fi
+	
+	# Clean up old files
+	rm -rf ../output/${algo_tuple}
+	rm -f ../output/${algo_tuple}*
+
+	IFS='-' read -r -a parts <<< "$algo_tuple"
+
+	algo=${parts[0]}
+	iterations=${parts[1]}
+
+	for SUPERPIXEL in "${SUPERPIXELS[@]}"
+	do
+		echo "${algo_tuple} _ SP=$SUPERPIXEL"
+		# Run the algo
+		if [ "$algo" == "slic" ]; then
+
+		    ../bin/${algo}_cli ../data/BSDS500/images/test/ --superpixels $SUPERPIXEL -o ../output/${algo_tuple}/$SUPERPIXEL -w --iterations $iterations
+
+		elif [ "$algo" == "customslic" ]; then
+
+			../bin/${algo}_cli ../data/BSDS500/images/test/ \
+				--superpixels $SUPERPIXEL -o ../output/${algo_tuple}/$SUPERPIXEL -w \
+				--iterations $iterations
+
+		fi
+
+		# Collect results
+	    ../bin/eval_summary_cli ../output/${algo_tuple}/$SUPERPIXEL ../data/BSDS500/images/test ../data/BSDS500/csv_groundTruth/test --append-file ../output/${algo_tuple}.csv --vis
+	    find ../output/${algo_tuple}/$SUPERPIXEL -type f -name '*[^summary|correlation|results].csv' -delete
+	done
+
+	# Average results
+	../bin/eval_average_cli ../output/${algo_tuple}.csv -o ../output/${algo_tuple}_average.csv
+
+done
+
+
+# run python script to create graphs of the comparison
+python ./make-graph.py ${algo_tuples_string}
--- examples/make-graph.py
+++ examples/make-graph.py
@@ -5,59 +5,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 parser = argparse.ArgumentParser(description='Generate SP algo comparison graphs')
-parser.add_argument('first', help='first algo name')
-parser.add_argument('second', help='second algo name')
+parser.add_argument('algo_tuple_list', help='Comma separated algo_tuples list')
 
 args = parser.parse_args()
 
 
-print(args.first)
-print(args.second)
+#print(args.algo_tuple_list)
+algo_tuple_list = [x.strip() for x in args.algo_tuple_list.split(',')]
 
-print ("Comparing " + args.first + " and " + args.second)
+print ("Comparing " + str (algo_tuple_list))
 
 reports_root = "../output/" 
-path_1 = reports_root + args.first + "_average.csv"
-path_2 = reports_root + args.second + "_average.csv"
+arrays_list = []
 
-first_array = []
-second_array = []
+for algo_tuple in algo_tuple_list:
 
-with open(path_1, 'rb') as csvfile:
-    csv_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+    path = reports_root + algo_tuple + "_average.csv"
+    array = []
 
-    for row in csv_data:
-        try:
-            first_array.append ([float(i) for i in row])
-        except: 
-            pass
+    with open(path, 'rb') as csvfile:
+        csv_data = csv.reader(csvfile, delimiter=',', quotechar='|')
 
-with open(path_2, 'rb') as csvfile:
-    csv_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in csv_data:
+            try:
+                array.append ([float(i) for i in row])
+            except: 
+                pass
 
-    for row in csv_data:
-        try:
-            second_array.append ([float(i) for i in row])
-        except: 
-            pass
+    array = np.array(array).astype(np.float)
+    arrays_list.append (array)
 
-first_array = np.array(first_array).astype(np.float)
-second_array = np.array(second_array).astype(np.float)
+#print (arrays_list)
 
+# K   Rec 1 - UE  EV 
 
-# for row in spamreader:
-#     print ', '.join(row)
-
-
-# K   Rec 1 - UE  EV
-
-def plot_value (first_arr, second_arr, index, title, y_label, savepng):
-    global args
+def plot_value (arrays, index, title, y_label, savepng):
+    global algo_tuple_list
 
     f = plt.figure(index)
-    plt.plot( first_arr [:,0], first_arr [:,index], 'o-', label=args.first)
-    plt.plot(second_arr [:,0], second_arr [:,index], '-x', label=args.second) 
+    ptr=0
+    for arr in arrays:
 
+        plt.plot(arr [:,0], arr [:,index], 'x-', label=algo_tuple_list[ptr])
+
+        ptr += 1
 
     plt.xlabel('Number of SPs')
     plt.ylabel(y_label)
@@ -70,11 +61,8 @@ def plot_value (first_arr, second_arr, index, title, y_label, savepng):
     plt.legend()
 
 
-
-
-
-plot_value (first_array, second_array, 1, "Boundry Recall", "REC", False)
-plot_value (first_array, second_array, 2, "Undersegmentation Error", "1 - UE", False)
-plot_value (first_array, second_array, 3, "Explained Variation", "EV", False)
+plot_value (arrays_list, 1, "Boundry Recall", "REC", False)
+plot_value (arrays_list, 2, "Undersegmentation Error", "1 - UE", False)
+plot_value (arrays_list, 3, "Explained Variation", "EV", False)
 
 plt.show()
